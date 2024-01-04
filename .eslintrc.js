4df117ad0b3be80ebcdc88dba2b65c689a1f45e2
HenryCodesGit <153534511+HenryCodesGit@users.noreply.github.com>
2024-01-04 16:16:49 -0500

    Fix dependency requirement for Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,10 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["airbnb-base", "prettier"],
+  extends: [
+    "airbnb-base",
+    "prettier",
+  ],
   overrides: [
     {
       env: {
