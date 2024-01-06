3bfa660af60dda0d24317ed41bd2056ca849c357
HenryCodesGit <153534511+HenryCodesGit@users.noreply.github.com>
2024-01-05 22:59:36 -0500

    Add ability for CPU to send data about attack to callback function

--- src/js/ComputerPlayer.js
+++ src/js/ComputerPlayer.js
@@ -16,15 +16,21 @@ export default class ComputerPlayer extends Player{
 
     // Randomly select a spot. If the attack returns as invalid, just try again
     // Technically this is O(infinity) if it ranomly happens to never pick the right value
-    attack(boardSIZE = this.#boardSize){
+    attack(boardSIZE = this.#boardSize, callbackFunction = ()=>{}){
         const getRandom = () => parseInt(Math.random()*boardSIZE, 10);
 
         let result = false;
+        let x;
+        let y;
         while(!result){
-            result = super.attack(getRandom(),getRandom());
+            x = getRandom();
+            y = getRandom();
+            result = super.attack(x,y);
         }
         
         this.#spacesChecked += 1;
+
+        callbackFunction(x,y,result);
         return result; //(this.#spacesChecked < this.#boardSize*this.#boardSize);
     }
 }
